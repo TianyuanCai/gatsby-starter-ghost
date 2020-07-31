adc624d19e56963154d26ee91018c39891fe9b9a
tcai <tycai47@gmail.com>
2020-07-31 08:43:27 -0700

    sitete config to https

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `http://condescending-carson-8177e4.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://condescending-carson-8177e4.netlify.app`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
